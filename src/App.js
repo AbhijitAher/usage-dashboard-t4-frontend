ff6a0b6264f0fd5ac7348600f7d79d337b4f8049
Abhijit <abhijit.aher@freshvnf.com>
2022-11-12 16:39:39 +0530

    implemented basic chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Chart from './components/Chart'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to your App usage Statistics</h1>
+      <Chart />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Chart.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+import { Chart, Bar } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+  LineElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  PieController,
+  LineController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle,
+} from 'chart.js'
+
+ChartJS.register(
+  BarElement,
+  LinearScale,
+  CategoryScale
+  /*
+    BarController,
+    ArcElement,
+    LineElement,
+    PointElement,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip,
+    SubTitle
+    */
+)
+
+export default function chart() {
+  const labels = [
+    'Other',
+    'Netflix',
+    'Slack',
+    'Hotstar',
+    'Skype-MicrosoftTeams',
+    'Dropbox',
+    'AmazonPrime',
+    // 'Facebook-Instagram',
+    // 'AppleMusic',
+    // 'Whatsapp',
+    // 'Youtube',
+  ]
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 45],
+        fill: false,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)',
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  }
+
+  return (
+    <div className="chart-div">
+      <Bar height={100} data={data} options={options} />
+    </div>
+  )
+}
